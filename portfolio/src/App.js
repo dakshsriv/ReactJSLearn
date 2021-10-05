717384ef40a01bd29d7560804a0e940d5a23bf3e
Daksh Srivastava <daksh.srivastava@gmail.com>
2021-10-05 19:07:47 -0400

    Manual building in progress

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Daksh. I'm a young mathematician and scientist.</p>
+                <p>I like working on projects.</p>
+            </div>
+        )
+    }
+}
+
+export default App;
